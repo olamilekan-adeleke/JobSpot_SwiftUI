df19b52dacfc60cf15dce8bb12d917d2b0cc4b85
Kod Enigma <olamilekanadeleke123@gmail.com>
2024-02-04 18:24:22 +0100

    Initial Commit

---
+++ JobSpot_SwiftUI/JobSpot_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  JobSpot_SwiftUIApp.swift
+//  JobSpot_SwiftUI
+//
+//  Created by Enigma Kod on 04/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct JobSpot_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
