c6bd8a49962291c1a4b2695a3fddad0f8fa90c1d
Kod Enigma <olamilekanadeleke123@gmail.com>
2024-02-14 19:35:26 +0100

    update

--- JobSpot_SwiftUI/Features/Auth/Login/View/LoginView.swift
+++ JobSpot_SwiftUI/Features/Auth/Login/View/LoginView.swift
@@ -12,39 +12,44 @@ struct LoginView: View {
     @State private var passwordText = ""
 
     var body: some View {
-        VStack {
-            Group {
-                TextView(text: "Welcome Back", size: 30, weight: .semibold)
-                    .frame(maxWidth: .infinity)
-                TextView(text: "Lorem ipsum dolor sit amet, consectetur adipiscing \nelit, sed do eiusmod tempor", size: 12, weight: .light)
-                    .opacity(0.7)
-                    .multilineTextAlignment(.center)
-                    .frame(maxWidth: .infinity)
+        NavigationStack {
+            VStack {
+                Group {
+                    TextView(text: "Welcome Back", size: 30, weight: .semibold)
+                        .frame(maxWidth: .infinity)
+                    TextView(text: "Lorem ipsum dolor sit amet, consectetur adipiscing \nelit, sed do eiusmod tempor", size: 12, weight: .light)
+                        .opacity(0.7)
+                        .multilineTextAlignment(.center)
+                        .frame(maxWidth: .infinity)
+                }
+
+                Spacer().frame(height: 60)
+
+                Group {
+                    BaseTextField(title: "Email", text: $emailText)
+                    Spacer().frame(height: 20)
+                    BaseTextField(title: "Password", text: $passwordText)
+                    TextView(text: "Forgot password", size: 11, weight: .light)
+                        .opacity(0.7)
+                        .frame(maxWidth: .infinity, alignment: .trailing)
+                }
+
+                Spacer().frame(height: 40)
+
+                Group {
+                    BaseButton(config: .init(title: "Login", type: .primary)) {}
+                    Spacer().frame(height: 10)
+                    BaseButton(config: .init(title: "Login With Google", type: .secondary)) {}
+                    Spacer().frame(height: 16)
+
+                    NavigationLink { VStack {} } label: {
+                        TextView(attributedString: createAccountText(), size: 10, weight: .light)
+                    }
+                }
             }
-
-            Spacer().frame(height: 60)
-
-            Group {
-                BaseTextField(title: "Email", text: $emailText)
-                Spacer().frame(height: 20)
-                BaseTextField(title: "Password", text: $passwordText)
-                TextView(text: "Forgot password", size: 11, weight: .light)
-                    .opacity(0.7)
-                    .frame(maxWidth: .infinity, alignment: .trailing)
-            }
-
-            Spacer().frame(height: 40)
-
-            Group {
-                BaseButton(config: .init(title: "Login", type: .primary)) {}
-                Spacer().frame(height: 10)
-                BaseButton(config: .init(title: "Sign Up", type: .secondary)) {}
-                Spacer().frame(height: 16)
-                TextView(attributedString: createAccountText(), size: 10, weight: .light)
-            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+            .padding([.horizontal, .vertical], 16)
         }
-        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
-        .padding([.horizontal, .vertical], 16)
     }
 
     private func createAccountText() -> AttributedString {
